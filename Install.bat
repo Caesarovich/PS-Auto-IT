88533016a7789d1215f038f03b12a066b10ad14b
Caesarovich <38408878+Caesarovich@users.noreply.github.com>
2021-02-03 20:14:06 +0100

    Adding reworked files
    I reworked the structure

--- Install.bat
+++ Install.bat
@@ -1,11 +1,9 @@
-:: This scripts copy the files from a USB stick onto the desktop
+:: This scripts copy the files from a USB stick onto the Desktop
 :: It then runs the start script
 
 @echo off
-Color 3F
+Color 1F
 
 powershell.exe -ExecutionPolicy Bypass -file "scripts/install.ps1"
-
-start "Start script" cmd /c %destinationDir%\Start.bat
 
 exit
---
+++ scripts/modules/set-wallpaper.ps1
@@ -0,0 +1,30 @@
+#########################
+
+# Auto-IT: Set Wallpaper
+# Description: Defines a custome image as the desktop wallpaper
+#   This is useful if your company replaces the default Windows' wallpaper by a branding one
+#
+# Author(s): Caesarovich
+# Version: 1.1.1
+
+##########################
+$host.UI.RawUI.WindowTitle = "Changing the Wallpaper"
+
+# Change that according to your needs
+# Place the image file in the root folder of Auto-IT
+$wallpaperFileName = "wp.png"
+
+$curDir = Get-Location
+$options = Get-Content "$curDir\options.json" | ConvertFrom-Json
+
+if ($options["ShouldChangeWP"] -eq "Yes") {
+  Set-ItemProperty -Path "HKCU:Control Panel\Desktop" -Name WallPaper -Value "$destinationDir\$wallpaperFileName"
+  rundll32.exe user32.dll, UpdatePerUserSystemParameters
+  Write-Host -ForegroundColor Green -Object "`n> Changing the wallpaper`n"
+
+  Add-Content -Path "$curDir\log.txt" -Value "[Ok] Wallpaper"
+}
+else {
+  Write-Host -ForegroundColor Green -Object "`n> Wallpaper change disabled.`n"
+  Add-Content -Path "$curDir\log.txt" -Value "[X] Wallpaper"
+}
